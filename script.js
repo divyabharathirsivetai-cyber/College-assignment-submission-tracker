93d23fef2dc72c5c61722fde0e696de67ae267d2
divyabharathirsivetai-cyber <divyabharathirsivetai@gmail.com>
2026-06-24 13:49:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let assignments = [];
 
-function addAssignment() {
+function addAssignment(){
 
     const title = document.getElementById("title").value;
     const subject = document.getElementById("subject").value;
@@ -51,39 +51,38 @@ function displayAssignments(data){
             <td>${item.title}</td>
             <td>${item.subject}</td>
             <td>${item.dueDate}</td>
-            <td class="${className}">
-                ${item.status}
-            </td>
-        </tr>
-        `;
+            <td class="${className}">${item.status}</td>
+        </tr>`;
     });
 }
 
 function updateDashboard(){
 
     document.getElementById("total").innerText =
-        assignments.length;
+    assignments.length;
 
     document.getElementById("submitted").innerText =
-        assignments.filter(a => a.status === "Submitted").length;
+    assignments.filter(a => a.status === "Submitted").length;
 
     document.getElementById("pending").innerText =
-        assignments.filter(a => a.status === "Pending").length;
+    assignments.filter(a => a.status === "Pending").length;
 
     document.getElementById("late").innerText =
-        assignments.filter(a => a.status === "Late").length;
+    assignments.filter(a => a.status === "Late").length;
 }
 
 function filterAssignments(){
 
-    const subject =
-    document.getElementById("filterSubject")
-    .value
-    .toLowerCase();
+    const status =
+    document.getElementById("filterStatus").value;
+
+    if(status === ""){
+        displayAssignments(assignments);
+        return;
+    }
 
     const filtered =
-    assignments.filter(a =>
-    a.subject.toLowerCase().includes(subject));
+    assignments.filter(a => a.status === status);
 
     displayAssignments(filtered);
 }
